9c87177a6838838f5c52aab62948f356bd7c3f88
Gurjant Singh <sandalgurjant@gmail.com>
2019-02-01 18:50:37 +0530

    Add files via upload

---
+++ performRecognition.py
@@ -0,0 +1,58 @@
+
+import cv2
+from sklearn.externals import joblib
+from skimage.feature import hog
+import numpy as np
+#import argparse as ap
+import tensorflow as tf
+
+cap = cv2.VideoCapture(0)
+#parser = ap.ArgumentParser()
+#parser.add_argument("-c", "--classiferPath", help="Path to Classifier File", required="True")
+#parser.add_argument("-i", "--image", help="Path to Image", required="True")
+#args = vars(parser.parse_args())
+
+
+#clf,pp = joblib.load(args["classiferPath"])
+clf,pp = joblib.load('digits_cls.pkl')
+#clf = tf.keras.models.load_model(args["classiferPath"])
+
+#im = cv2.imread(args["image"])
+while True:
+    ret, im = cap.read()
+
+    im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    im_gray = cv2.GaussianBlur(im_gray, (5, 5), 0)
+
+
+    ret, im_th = cv2.threshold(im_gray, 90, 255, cv2.THRESH_BINARY_INV)
+
+
+    _,ctrs, hier = cv2.findContours(im_th.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+     
+    rects = [cv2.boundingRect(ctr) for ctr in ctrs]
+
+
+    for rect in rects:
+      
+        cv2.rectangle(im, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 3) 
+      
+        leng = int(rect[3] * 1.6)
+        pt1 = int(rect[1] + rect[3] // 2 - leng // 2)
+        pt2 = int(rect[0] + rect[2] // 2 - leng // 2)
+        roi = im_th[pt1:pt1+leng, pt2:pt2+leng]
+      
+        roi = cv2.resize(roi, (28, 28), interpolation=cv2.INTER_AREA)
+        roi = cv2.dilate(roi, (3, 3))
+      
+        roi_hog_fd = hog(roi, orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), visualise=False)
+        roi_hog_fd = pp.transform(np.array([roi_hog_fd], 'float64'))
+        nbr = clf.predict(roi_hog_fd)
+        cv2.putText(im, str(int(nbr[0])), (rect[0], rect[1]),cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 255), 3)
+
+    cv2.namedWindow("Resulting Image with Rectangular ROIs", cv2.WINDOW_NORMAL)
+    cv2.imshow("Resulting Image with Rectangular ROIs", cv2.resize(im,(800,600)))
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
